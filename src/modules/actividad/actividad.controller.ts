12ba3f4c3998396a4b6628e7b77d39ccb1c20f71
Duvan Jamid Vargas <duvan.vargas@mercadolibre.com.co>
2022-12-28 15:40:05 -0500

    se hace servicio para guardar preguntas

--- src/modules/actividad/actividad.controller.ts
+++ src/modules/actividad/actividad.controller.ts
@@ -1,32 +1,39 @@
 import {Controller, Get, Post, Param, Body, Delete} from "@nestjs/common";
-import { Usuario } from "../../entities/usuario.entitie";
-import { ActividadService } from "./actividad.service";
-import { Actividad } from "../../entities/actividad.entitie";
-import { ActividadInterface } from "./actividad.interface";
+import {Usuario} from "../../entities/usuario.entitie";
+import {ActividadService} from "./actividad.service";
+import {Actividad} from "../../entities/actividad.entitie";
+import {ActividadInterface, PreguntaInterface} from "./actividad.interface";
+import {Pregunta} from "../../entities/pregunta.entitie";
 
 @Controller("actividad")
 export class ActividadController {
-  constructor(private readonly actividadService: ActividadService) {}
+    constructor(private readonly actividadService: ActividadService) {
+    }
 
-  @Get()
-  getAll(): Promise<Actividad[]> {
-    return this.actividadService.findAll();
-  }
+    @Get()
+    getAll(): Promise<Actividad[]> {
+        return this.actividadService.findAll();
+    }
 
-  @Get('/:id')
-  getId(@Param("id") id:number): Promise<Actividad> {
-    return this.actividadService.findOne(id);
-  }
+    @Get('/:id')
+    getId(@Param("id") id: number): Promise<Actividad> {
+        return this.actividadService.findOne(id);
+    }
 
-  @Post()
-  save(@Body() actividad:ActividadInterface): Promise<Actividad> {
-    return this.actividadService.save(actividad);
-  }
-  
-  @Delete('/:id')
-  eliminar(@Param("id") id:number): Promise<any>{
-    return this.actividadService.remove(id);
-  }
+    @Post()
+    save(@Body() actividad: ActividadInterface): Promise<Actividad> {
+        return this.actividadService.save(actividad);
+    }
 
+    @Delete('/:id')
+    eliminar(@Param("id") id: number): Promise<any> {
+        return this.actividadService.remove(id);
+    }
+
+
+    @Post('/:idActividad/pregunta')
+    savePregunta(@Param("idActividad") idActividad: number, @Body() pregunta: PreguntaInterface): Promise<Pregunta> {
+        return this.actividadService.savePregunta(idActividad, pregunta)
+    }
 
 }
--- src/modules/actividad/actividad.service.ts
+++ src/modules/actividad/actividad.service.ts
@@ -1,77 +1,99 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { Actividad, ETipoActividad } from "../../entities/actividad.entitie";
-import { ActividadInterface } from "./actividad.interface";
-import { Usuario } from "../../entities/usuario.entitie";
-import { Pregunta } from "../../entities/pregunta.entitie";
+import {Injectable} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {Actividad, ETipoActividad} from "../../entities/actividad.entitie";
+import {ActividadInterface, PreguntaInterface} from "./actividad.interface";
+import {Usuario} from "../../entities/usuario.entitie";
+import {Pregunta} from "../../entities/pregunta.entitie";
 
 
 @Injectable()
 export class ActividadService {
-  constructor(
-    @InjectRepository(Actividad)
-    private repository: Repository<Actividad>,
-    @InjectRepository(Usuario)
-    private repositoryUsuario: Repository<Usuario>
-  ) {
-  }
-
-  findAll(): Promise<Actividad[]> {
-    return this.repository.find({
-        relations: {
-          usuario: true,
-          preguntas: true
-        }
-      }
-    );
-  }
-
-  findOne(id: number): Promise<Actividad> {
-    return this.repository.findOneBy({ id: id });
-  }
-
-  async remove(id: number): Promise<any> {
-   
-    await this.repository.delete(id);
-    
-    //let actividad = this.repository.findOneBy({id:id});
-    //this.repository.remove(actividad);
-  }
-
-  async save(actividad: ActividadInterface): Promise<Actividad> {
-
-    let actividadEntity: Actividad = this.repository.create();
-    actividadEntity.nombre = actividad.nombre;
-    actividadEntity.duracion = actividad.duracion;
-    actividadEntity.fecha_apertura = actividad.fecha_apertura;
-    actividadEntity.fecha_cierre = actividad.fecha_cierre;
-    actividadEntity.ponderacion = actividad.ponderacion;
-
-    await this.repositoryUsuario
-      .findOneBy({ id: actividad.usuarioId })
-      .then(value => actividadEntity.usuario = value);
-
-    if (actividad.tipo.toUpperCase() == ETipoActividad.QUIZ.valueOf()) {
-      actividadEntity.tipo = ETipoActividad.QUIZ;
-
-    } else {
-      actividadEntity.tipo = ETipoActividad.TALLER;
+    constructor(
+        @InjectRepository(Actividad)
+        private repository: Repository<Actividad>,
+        @InjectRepository(Usuario)
+        private repositoryUsuario: Repository<Usuario>,
+        @InjectRepository(Pregunta)
+        private repositoryPregunta: Repository<Pregunta>
+    ) {
     }
-    actividadEntity.preguntas = [];
 
-    actividad.preguntas.forEach((value) => {
-      let pregunta: Pregunta = Pregunta.create();
-      pregunta.nombre = value.nombre;
-      pregunta.ponderacion = value.ponderacion;
-      pregunta.usuario = actividadEntity.usuario;
+    findAll(): Promise<Actividad[]> {
+        return this.repository.find({
+                relations: {
+                    usuario: true,
+                    preguntas: true
+                }
+            }
+        );
+    }
 
-      actividadEntity.preguntas.push(pregunta);
-    });
+    findOne(id: number): Promise<Actividad> {
+        return this.repository.findOneBy({id: id});
+    }
+
+    async remove(id: number): Promise<any> {
+
+        await this.repository.delete(id);
+
+        //let actividad = this.repository.findOneBy({id:id});
+        //this.repository.remove(actividad);
+    }
+
+    /**
+     * metodo para guardar una activida en base de datos, incluida las preguntas
+     * @param actividad
+     */
+    async save(actividad: ActividadInterface): Promise<Actividad> {
+
+        let actividadEntity: Actividad = this.repository.create();
+        actividadEntity.nombre = actividad.nombre;
+        actividadEntity.duracion = actividad.duracion;
+        actividadEntity.fecha_apertura = actividad.fecha_apertura;
+        actividadEntity.fecha_cierre = actividad.fecha_cierre;
+        actividadEntity.ponderacion = actividad.ponderacion;
+
+        await this.repositoryUsuario
+            .findOneBy({id: actividad.usuarioId})
+            .then(value => actividadEntity.usuario = value);
+
+        if (actividad.tipo.toUpperCase() == ETipoActividad.QUIZ.valueOf()) {
+            actividadEntity.tipo = ETipoActividad.QUIZ;
+
+        } else {
+            actividadEntity.tipo = ETipoActividad.TALLER;
+        }
+        actividadEntity.preguntas = [];
+
+        actividad.preguntas.forEach((value) => {
+            let pregunta: Pregunta = Pregunta.create();
+            pregunta.nombre = value.nombre;
+            pregunta.ponderacion = value.ponderacion;
+            pregunta.usuario = actividadEntity.usuario;
+
+            actividadEntity.preguntas.push(pregunta);
+        });
 
 
-    return this.repository.save(actividadEntity);
-  }
+        return this.repository.save(actividadEntity);
+    }
 
+    /**
+     * metodo para guardar una  pregunta en base de datos
+     * @param actividad
+     */
+    async savePregunta(idActividad: number, pregunta: PreguntaInterface): Promise<Pregunta> {
+
+        let preguntaEntity: Pregunta = this.repositoryPregunta.create();
+        preguntaEntity.nombre = pregunta.nombre;
+        preguntaEntity.ponderacion = pregunta.ponderacion;
+
+        await this.repository
+            .findOneBy({id: idActividad})
+            .then(value => preguntaEntity.actividad = value);
+
+        return this.repositoryPregunta.save(preguntaEntity);
+    }
 
 }
